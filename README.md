da810504f9faa896b43d98015c7427139d2d3b24
petercz1 <email.peter.carroll@gmail.com>
2019-05-03 17:01:55 +0800

    first commit for Simple_PHP_router

---
+++ README.md
@@ -0,0 +1,9 @@
+# simple php router
+![](https://img.shields.io/badge/licence-free-green.svg) ![](https://img.shields.io/badge/frameworks-none-green.svg)
+
+About as simple as possible and still be useful
+
+
+new repo for 
+new repo for 
+new repo for Simple_PHP_router
---
+++ router.php
@@ -0,0 +1,35 @@
+<?php
+
+//simple no-framework skinny router
+
+(function () {
+    // these are the paths we want
+    $uri = [
+    '/'=>'home.html',
+    '/about'=>'about.html',
+    '/contact'=>'newperson.php'
+    ];
+
+    // if a route is matched, set this to true
+    $matched = false;
+
+    // split uri from any params - php puts them in the $_GET array
+    // this splits the path at the questionmark character
+    // and captures only the first part
+    $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 1);
+
+    // thischecks if there is a path, or directs to '/'
+    $route = $uri_parts[0] ?? '/';
+
+    foreach ($uri as $routeKey => $routeValue) {
+        if (preg_match("#^$routeKey$#", $route)) {
+            include $routeValue;
+            $matched = true;
+        }
+    }
+
+    // if no match, send 404
+    if (!$matched) {
+        include 'fourohfour.html';
+    }
+})();
